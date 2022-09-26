97949adc5d7699fa72ae52d2beb9c578166da62d
Estudiante <Estudiante@T203-11-2022>
2022-09-26 21:39:10 +0200

    Agregar archivos de proyecto.

---
+++ 8.0.0/L5 LPRR 1148822/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numeroEntero = 0;
+
+        Console.WriteLine("EJERCICIO 1");
+        Console.WriteLine("Nùmero Entero");
+
+        numeroEntero = Console.Read();
+    }
+}
---
+++ L5 LPRR 1148822/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numeroEntero =0;
+
+Console.WriteLine("EJERCICIO 1");
+Console.WriteLine("numero Entero");
+
+numeroEntero =  Convert.ToInt32(Console.ReadLine());
+
+if (numeroEntero >= 1)
+{
+    Console.WriteLine("Numero positivo");
+}
+if (numeroEntero <= -1)
+{
+    Console.WriteLine("Numero negativo");
+}
+if (numeroEntero == 0)
+{ 
+    Console.WriteLine("El numero que ingreso es cero");
+}
+
+int numeroDia;
+
+Console.WriteLine("EJERCICIO 2");
+Console.WriteLine("Igresar numero de dia");
+
+numeroEntero = Convert.ToInt32(Console.ReadLine());
+
+if()
+
+
